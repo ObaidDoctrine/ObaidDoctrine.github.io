dc377b92615740d256fd395b8b8e5f7afc890390
ObaidDoctrine <ObaidDoctrine@gmail.com>
2026-09-20 14:04:50 +0500

    Check Facebook Page task access

--- .github/scripts/publish-facebook.sh
+++ .github/scripts/publish-facebook.sh
@@ -16,7 +16,7 @@ fi
 
 ACCOUNTS_RESPONSE=$(curl --silent --show-error --write-out "\nHTTP_STATUS:%{http_code}" \
   --get \
-  --data-urlencode "fields=id,name,access_token" \
+  --data-urlencode "fields=id,name,tasks,access_token" \
   --data-urlencode "access_token=$FB_PAGE_ACCESS_TOKEN" \
   "https://graph.facebook.com/$FB_GRAPH_VERSION/me/accounts")
 
